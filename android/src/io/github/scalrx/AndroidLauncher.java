7d10f27fc5b3317f9c07e10416a79db698b2c87b
Seth Allen <scalrx@gmail.com>
2018-11-27 14:07:08 -0600

    Initial commit

---
+++ android/src/io/github/scalrx/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package io.github.scalrx;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import io.github.scalrx.KnyttStories;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new KnyttStories(), config);
+	}
+}
---
+++ desktop/src/io/github/scalrx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.scalrx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.scalrx.KnyttStories;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KnyttStories(), config);
+	}
+}
